8f56558ce4c46c7e045b49b9673e8ddff0c1e510
Maximilian Weber <weber.maximilian@googlemail.com>
2011-04-26 13:43:57 +0200

    New description.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject initiator "0.1.0-SNAPSHOT"
   :description "The initiator searchs in namespaces for variables (e.g. functions)
-                which has the specified metadata entry. Only namespaces with the
-                {:initiator true} metadata entry are included in the search"
+                which match a predicate. Only namespaces with the
+                {:initiator true} metadata entry are included in the search."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]]
